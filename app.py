0389b57e5d687d511f19e0f04dc80c063bf87f73
Theo Linnemann <theo@theolinnemann.com>
2019-08-26 15:01:07 -0400

    Add Anita's testimonial.

--- app.py
+++ app.py
@@ -144,6 +144,7 @@ app.layout = html.Div(id='app_container', className='container', children=[
                 html.H4('''"I'm blocking this number." - Mark'''),
                 html.H4('''"Hey wait, no really, please, you gotta stop." - Mij'''),
                 html.H4('''"3 years!?" - Jim'''),
+                html.H4('''"Wtf is this you weirdo." - Alex's Sister''')
             ], style={'textAlign': 'center'})
 
         ])
